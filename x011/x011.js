4eab86b294b913db6045b28940d599a3e90d738b
GustavoMarquesdeOliveira <82977208+GustavoMarquesdeOliveira@users.noreply.github.com>
2021-05-08 22:23:18 -0300

    Exercicios

---
+++ x011/x011.js
@@ -0,0 +1,15 @@
+var c = 1
+while(c < 6){
+    console.log('Tudo bem?')
+    c++
+}
+var a = 0
+while (a < 10){
+    console.log (`Oi meu número preferido é ${a}`)
+    a++
+}
+var b = 5
+do{
+    console.log(" Oi eu sou o do")
+    b++
+} while (b < 70)
